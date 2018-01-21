bd8eaec60efaad43cd1c3ea41a56b3df5d7987a1
Bhuwan Upadhyay <bhuwan.upadhyay49@gmail.com>
2018-01-21 22:21:57 +0400

    uaa-keycloak

--- knight.sh
+++ knight.sh
@@ -3,6 +3,7 @@
 function buildWithDockerImage() {
     echo "Clean and Build Project"
     ./gradlew clean build copyDocker docker -x test
+    cd foo-web-client && npm run build && cd ../
 }
 
 function publishToCloudfoundry() {
