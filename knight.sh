894e551618e7aff049f2a8f77821570c061f0da6
Bhuwan Upadhyay <bhuwan.upadhyay49@gmail.com>
2018-01-22 06:59:39 +0400

    uaa-keycloak

--- knight.sh
+++ knight.sh
@@ -2,7 +2,8 @@
 
 function buildWithDockerImage() {
     echo "Clean and Build Project"
-    ./gradlew clean build copyDocker docker -x test
+    ./gradlew clean build -x test
+    #./gradlew clean build copyDocker docker -x test
     cd foo-web-client && npm run build && cd ../
 }
 
